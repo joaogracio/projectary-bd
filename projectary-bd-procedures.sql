5b900a1e014f8142ad03af1be488a8163ae9ce84
razoes <razoes@users.noreply.github.com>
2017-04-25 23:21:20 +0100

    Updated some procs and added some others
    isAdmin (checks if given user is Admin, returns BOOLEAN)
    
    isteacher (checks if given user is Teacher, returns BOOLEAN)
    
    isStudent (checks if given user is Student, returns BOOLEAN)
    
    isInGroup (checks if given user is in a given group, returns BOOLEAN)
    
    isInProject (checks if given user is already in a project, returns
    BOOLEAN)
    
    addToGroup (checks several conditions to ensure correct insert, returns
    success BOOLEAN)
    
    insertNewGroup (checks if given user is not already in a project and
    only then inserts new group, returns groupid INT)
    
    insertNewApplication (checks several conditions to ensure correct
    insert, returns success BOOLEAN)

--- projectary-bd-procedures.sql
+++ projectary-bd-procedures.sql
@@ -1,37 +1,103 @@
-DROP PROCEDURE IF EXISTS API_InsertNewApplication;
-CREATE PROCEDURE API_InsertNewApplication(IN entity VARCHAR(255))
- BEGIN
-  DECLARE GEUUID VARCHAR(255);
-  DECLARE APPUUID VARCHAR(255);
-  DECLARE EFUNCUUID VARCHAR(255);
-  SELECT UUID() INTO GEUUID;
-  SELECT UUID() INTO APPUUID;
-  SELECT id FROM function WHERE `desc`="Student" INTO EFUNCUUID;
-  INSERT INTO groupentity (id,entity,function,createdin,createdby) VALUES (GEUUID,entity,EFUNCUUID,NOW(),entity);
-  INSERT INTO application VALUES (APPUUID,entity,NOW());
-  INSERT INTO applicationgroup VALUES (APPUUID,GEUUID,NOW(),entity);
-  SELECT APPUUID, GEUUID;
- END;
-
-DROP PROCEDURE IF EXISTS API_IsTeacher;
-CREATE PROCEDURE API_IsTeacher(IN entity VARCHAR(255))
+-- isAdmin --
+DROP PROCEDURE IF EXISTS isAdmin;
+DELIMITER $$
+CREATE PROCEDURE isAdmin(IN id INT, OUT isAdmin BOOL)
 BEGIN
-  DECLARE TEACHER BOOLEAN;
-  SELECT EXISTS(SELECT * FROM teacher AS t where t.id=entity) INTO TEACHER;
-  SELECT TEACHER;
-END;
+	SET isAdmin = (SELECT u.isadmin FROM user u WHERE u.id = id);
+END$$
+DELIMITER ;
 
-CREATE PROCEDURE API_IsStudent(IN entity VARCHAR(255))
-  BEGIN
-    DECLARE STUDENT BOOLEAN;
-    SELECT EXISTS(SELECT * FROM student AS s where s.id=entity) INTO STUDENT;
-    SELECT STUDENT;
-  END;
+-- isteacher --
+DROP PROCEDURE IF EXISTS isTeacher;
+DELIMITER $$
+CREATE PROCEDURE isTeacher(IN id INT, OUT isTeacher BOOL)
+BEGIN
+	SET isTeacher = (SELECT EXISTS(SELECT * FROM user u, type t WHERE u.id = id AND u.typeid = t.id AND t.`desc` LIKE "teacher"));
+END$$
+DELIMITER ;
 
-DROP PROCEDURE IF EXISTS IsInGroup;
-CREATE PROCEDURE IsInGroup(IN entityUUID VARCHAR(255), IN groupUUID VARCHAR(255))
-  BEGIN
-    DECLARE IS_IN_GROUP BOOLEAN;
-    SELECT EXISTS(SELECT * FROM groupentity ge WHERE ge.id=groupUUID AND ge.entity=entityUUID) INTO IS_IN_GROUP;
-    SELECT IS_IN_GROUP;
-  END;
+-- isStudent --
+DROP PROCEDURE IF EXISTS isStudent;
+DELIMITER $$
+CREATE PROCEDURE isStudent(IN id INT, OUT isStudent BOOL)
+BEGIN
+	SET isStudent = (SELECT EXISTS(SELECT * FROM user u, type t WHERE u.id = id AND u.typeid = t.id AND t.`desc` LIKE "student"));
+END$$
+DELIMITER ;
+
+-- isInGroup --
+DROP PROCEDURE IF EXISTS isInGroup;
+DELIMITER $$
+CREATE PROCEDURE isInGroup(IN userid INT, IN groupid INT, OUT isInGroup BOOL)
+BEGIN
+	SET isInGroup = (SELECT EXISTS(SELECT * FROM groupuser gu WHERE gu.userid = userid AND gu.groupid = groupid));
+END$$
+DELIMITER ;
+
+-- isInProject --
+DROP PROCEDURE IF EXISTS isInProject;
+DELIMITER $$
+CREATE PROCEDURE isInProject(IN userid INT, OUT isInProject BOOL)
+BEGIN
+	SET isInProject = (SELECT EXISTS(SELECT * FROM groupuser gu, application a WHERE gu.userid = userid AND gu.groupid = a.groupid AND YEAR(a.approvedin) != 0000));
+END$$
+DELIMITER ;
+
+-- addToGroup --
+DROP PROCEDURE IF EXISTS addToGroup;
+DELIMITER $$
+CREATE PROCEDURE addToGroup(IN userid INT, IN groupid INT, IN password VARCHAR(255), OUT state BOOL)
+BEGIN
+	SET state = FALSE;
+	CALL isInProject(userid, @project);
+	IF (@project = FALSE) THEN
+		CALL isInGroup(userid, groupid, @isInGroup);
+        IF (@isInGroup = FALSE) THEN
+			IF (SELECT EXISTS(SELECT * FROM `group` g WHERE g.id = groupid AND g.password = password)) THEN
+				INSERT INTO groupuser(groupid, userid)
+					VALUES (groupid, userid);
+                    SET state = TRUE;
+			END IF;
+		END IF;
+	END IF;
+END$$
+DELIMITER ;
+
+-- insertNewGroup --
+DROP PROCEDURE IF EXISTS insertNewGroup;
+DELIMITER $$
+CREATE PROCEDURE insertNewGroup(IN userid INT, IN description VARCHAR(255), IN password VARCHAR(255), OUT groupid INT)
+BEGIN
+	CALL isInProject(userid, @project);
+	IF (@project=FALSE) THEN
+		INSERT INTO `group`(`desc`, password)
+			VALUES (description, password);
+		SET groupid = (SELECT g.id FROM `group` g WHERE g.`desc` = description AND g.password = password);
+		INSERT INTO groupuser (groupid, userid)
+			VALUES (groupid, userid);
+	END IF;
+END$$
+DELIMITER ;
+
+-- insertNewApplication --
+DROP PROCEDURE IF EXISTS insertNewApplication;
+DELIMITER $$
+CREATE PROCEDURE insertNewApplication(IN userid INT, IN groupid INT, IN projectid INT, OUT state BOOL)
+BEGIN
+	SET state = FALSE;
+	CALL isStudent(userid, @student);
+	IF (@student = TRUE) THEN
+		CALL isInGroup(userid, groupid, @isInGroup);
+        IF (@isInGroup = TRUE) THEN
+			CALL isInProject(userid, @project);
+			IF (@project = FALSE) THEN
+				IF (SELECT EXISTS(SELECT * FROM project p WHERE p.id = projectid AND p.approvedin IS NOT NULL)) THEN
+					INSERT INTO application(groupid, projectid, submitedin)
+						VALUES (groupid, projectid, NOW());
+						SET state = TRUE;
+				END IF;
+			END IF;
+		END IF;
+	END IF;
+END$$
+DELIMITER ;
