184594d09f27b15166ca6bc789d970c0a351d67f
razoes <razoes@users.noreply.github.com>
2017-04-28 00:53:31 +0100

    Deleted duplicated procedures
    Some procedures were duplicated. Deleted older ones.

--- projectary-bd-procedures.sql
+++ projectary-bd-procedures.sql
@@ -1,8 +1,3 @@
--- isAdmin --
-DROP PROCEDURE IF EXISTS isAdmin;
-DELIMITER $$
-
-<<<<<<< myProcs_3
 CREATE PROCEDURE isAdmin(IN id INT, OUT isAdmin BOOL)
 BEGIN
 	SET isAdmin = (SELECT u.isadmin FROM user u WHERE u.id = id);
@@ -109,38 +104,3 @@ BEGIN
 	END IF;
 END$$
 DELIMITER ;
-=======
-CREATE PROCEDURE isAdmin(IN id INT)
-BEGIN
-	SELECT u.isadmin FROM user u WHERE u.id = id;
-END$$
-DELIMITER ;
-
--- isteacher --
-DROP PROCEDURE IF EXISTS isTeacher;
-DELIMITER $$
-CREATE PROCEDURE isTeacher(IN id INT)
-BEGIN
-	SELECT EXISTS(SELECT * FROM user u, type t WHERE u.id = id AND u.typeid = t.id AND t.`desc` LIKE "teacher");
-END$$
-DELIMITER ;
-
--- isStudent --
-DROP PROCEDURE IF EXISTS isStudent;
-DELIMITER $$
-CREATE PROCEDURE isStudent(IN id INT)
-BEGIN
-	SELECT EXISTS(SELECT * FROM user u, type t WHERE u.id = id AND u.typeid = t.id AND t.`desc` LIKE "student");
-END$$
-DELIMITER ;
-
--- isInGroup --
-DROP PROCEDURE IF EXISTS isInGroup;
-DELIMITER $$
-CREATE PROCEDURE isInGroup(IN userid INT, IN groupid INT)
-BEGIN
-	SELECT EXISTS(SELECT * FROM groupuser gu WHERE gu.userid = userid AND gu.groupid = groupid);
-END$$
-DELIMITER ;
->>>>>>> master
-
